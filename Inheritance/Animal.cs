8eb33ab0c297b601771092596f6bae91d6403ab3
ahmad <bha296780@gmail.com>
2022-09-25 18:07:45 -0400

    finally

---
+++ Inheritance/Animal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    public class Animal
+    {
+        public int eyes;
+        public string name;
+        public string color;
+        public int ears;
+
+    }
+    public class Bird : Animal
+    {
+        public int feet;
+        public int wings;
+        public string voice;
+        public double flyingHeight;
+    }
+    public class Reptile : Animal
+    {
+        public int backbone;
+        public double weight;
+        public string skinColor;
+        public string eyeColor;
+    }   
+    
+}    
--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -34,6 +34,33 @@ namespace Inheritance
              *  
              * Creatively display the class member values 
              */
+            Bird bird1 = new Bird();
+            bird1.feet = 2;
+            bird1.wings = 3;
+            bird1.voice = "ded";
+            bird1.flyingHeight = 22;
+            bird1.ears = 2;
+            bird1.color = "brown";
+            bird1.eyes = 2;
+            bird1.name = "eagle";
+            Console.WriteLine($"{bird1.name},{bird1.feet},{bird1.wings},{bird1.color},{bird1.ears},{bird1.voice}," +
+                $"{bird1.flyingHeight},{bird1.eyes}");
+            Console.WriteLine("-----------");
+            Reptile reptile1 = new Reptile();
+            reptile1.weight = 2;
+            reptile1.backbone = 3;
+            reptile1.skinColor = "dark grey";
+            reptile1.eyeColor = "red";
+            reptile1.ears = 2;
+            reptile1.color = "brown";
+            reptile1.eyes = 2;
+            reptile1.name = "snake";
+            Console.WriteLine(reptile1.ears);
+            Console.WriteLine(reptile1.color);
+            Console.WriteLine(reptile1.eyes);
+            Console.WriteLine(reptile1.name);
+
+
         }
     }
 }
